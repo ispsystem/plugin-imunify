bd475bdcb0eac9901198a1619ad5ce325f88aef5
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-06-19 15:35:27 +0800

    feat(antivirus-card): create preview

---
+++ packages/antivirus-card/src/preview/lock.icon.ts
@@ -0,0 +1,34 @@
+import { AbstractElement, Define } from 'abstract-element';
+import litRender from 'abstract-element/render/lit';
+import { svg, TemplateResult } from 'lit-html';
+
+/**
+ * The imunifyav-card web component
+ */
+@Define('plugin-imunifyav-card-icon__lock')
+export default class IconLock extends AbstractElement<TemplateResult> {
+  constructor() {
+    super(litRender);
+  }
+
+  render() {
+    return svg`
+      <svg width="11" height="13" viewBox="0 0 11 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="2.01865" y="0.65" width="6.22632" height="8.96316" rx="3.11316" stroke="#9B9B9B" stroke-width="1.3" />
+        <path
+          d="M0.65 8.10522C0.65 6.25507 2.14985 4.75522 4 4.75522H6.26316C8.11331 4.75522 9.61316 6.25507 9.61316 8.10522V11C9.61316 11.7455 9.00874 12.35 8.26316 12.35H2C1.25442 12.35 0.65 11.7455 0.65 11V8.10522Z"
+          fill="white"
+          stroke="#979797"
+          stroke-width="1.3"
+        />
+        <path d="M5.13178 7.86847V9.9211" stroke="#9B9B9B" stroke-linecap="round" />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M5.13178 8.89467C5.6986 8.89467 6.1581 8.43518 6.1581 7.86836C6.1581 7.30154 5.6986 6.84204 5.13178 6.84204C4.56497 6.84204 4.10547 7.30154 4.10547 7.86836C4.10547 8.43518 4.56497 8.89467 5.13178 8.89467Z"
+          fill="#9B9B9B"
+        />
+      </svg>
+    `;
+  }
+}
