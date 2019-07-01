3d2d7c814fa6b316796cf3af84caae40a0d08181
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-07-01 14:15:19 +0800

    feat: 🎸 add uuidv4 function to utils tools

--- packages/antivirus-card/src/utils/tools.ts
+++ packages/antivirus-card/src/utils/tools.ts
@@ -34,3 +34,14 @@ export function declOfNum(num: number, listMsgs: string[]): string {
     }
   }
 }
+
+/**
+ * Создание уникального идентификатора
+ * https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+ */
+export function uuidv4() {
+  const hash: any = ([1e7] as any) + -1e3 + -4e3 + -8e3 + -1e11;
+  // https://stackoverflow.com/questions/44042816/what-is-wrong-with-crypto-getrandomvalues-in-internet-explorer-11
+  const crypto = window.crypto || window['msCrypto']; // for IE 11
+  return hash.replace(/[018]/g, c => (c ^ (crypto.getRandomValues(new Uint8Array(1))![0] & (15 >> (c / 4)))).toString(16));
+}
