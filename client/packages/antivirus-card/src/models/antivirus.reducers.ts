b8415677dac0d54c2a168e5644174e0ec667f873
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-07-27 16:59:46 +0800

    refactor: 💡 move interface to separate file
    Closes: #AS-1490

--- client/packages/antivirus-card/src/models/antivirus.reducers.ts
+++ client/packages/antivirus-card/src/models/antivirus.reducers.ts
@@ -1,106 +1,5 @@
 import { AntivirusActionTypes, ANTIVIRUS_ACTION } from './antivirus.actions';
-
-/**
- * Infected file
- */
-interface InfectedFile {
-  id: number;
-  // file name, e.g. "beregovoi_orcestr.bat"
-  name: string;
-  // file status, e.g. "заражён"
-  status: 'INFECTED' | 'CURED' | 'EXCEPTED' | 'HEALING';
-  // type of threat, e.g. "Troyan.enspect"
-  threatName: string;
-  // path to file
-  path: string;
-  // date and time when the file was detected (timestamp)
-  detectionDate: number;
-  // date and time when the file was created (timestamp)
-  createdDate: number;
-  // date and time when the file was changed (timestamp)
-  lastChangeDate?: number;
-}
-
-/**
- * History list item
- */
-interface HistoryItem {
-  // verification date and time (timestamp)
-  date: number;
-  // check type
-  checkType: 'FULL' | 'PARTIAL';
-  // count infected files
-  infectedFilesCount: number;
-  // count cured files
-  curedFilesCount: number;
-
-  scanOptionId: number;
-}
-
-/**
- * Scan option list item
- */
-export interface ScanOption {
-  id: number;
-  path: string[];
-  checkMask: string[];
-  excludeMask: string[];
-  intensity: 'LOW' | 'MEDIUM' | 'HIGH';
-  scheduleTime: {
-    daily?: {
-      hour: number;
-      minutes: number;
-    };
-    single?: {
-      date: number;
-    };
-    weekly?: {
-      day: number;
-      time: {
-        hour: number;
-        minutes: number;
-      };
-    };
-    monthly?: {
-      day: number;
-      time: {
-        hour: number;
-        minutes: number;
-      };
-    };
-  };
-  checkFileTypes: 'CRITICAL' | 'ALL' | 'EXCEPT_MEDIA';
-  saveCopyFilesDay: number;
-  cureFoundFiles: boolean;
-  removeInfectedFileContent: boolean;
-  checkDomainReputation: boolean;
-  email: string;
-  parallelChecks: number;
-  ramForCheck: number;
-  fullLogDetails: boolean;
-  maxScanTime: number;
-  autoUpdate: boolean;
-}
-
-/**
- * Global antivirus state
- */
-export interface AntivirusState {
-  // global state error
-  error: any;
-
-  // features
-  isProVersion: boolean;
-  hasScheduledActions: boolean;
-
-  // scanning flag
-  scanning: boolean;
-  // domain in black list
-  inBlackLists: boolean;
-
-  infectedFiles: InfectedFile[];
-  history: HistoryItem[];
-}
+import { AntivirusState } from './antivirus.model';
 
 const getInitialState = (): AntivirusState => {
   return {
@@ -112,7 +11,7 @@ const getInitialState = (): AntivirusState => {
     scanning: false,
     infectedFiles: [],
     inBlackLists: false,
-    history: []
+    history: [],
   };
 };
 
@@ -122,7 +21,7 @@ export const antivirusReducer = (state: AntivirusState = getInitialState(), acti
       return {
         ...state,
         scanning: true,
-        error: null
+        error: null,
       };
     }
 
@@ -130,7 +29,7 @@ export const antivirusReducer = (state: AntivirusState = getInitialState(), acti
       return {
         ...state,
         scanning: true,
-        error: null
+        error: null,
       };
     }
 
@@ -140,8 +39,8 @@ export const antivirusReducer = (state: AntivirusState = getInitialState(), acti
         scanning: false,
         infectedFiles: [
           // merge infected files with new scan result
-          ...new Map((state.infectedFiles || []).concat(action.payload.data).map(item => [item.id, item])).values()
-        ]
+          ...new Map((state.infectedFiles || []).concat(action.payload.data).map(item => [item.id, item])).values(),
+        ],
       };
     }
 
@@ -149,28 +48,28 @@ export const antivirusReducer = (state: AntivirusState = getInitialState(), acti
       return {
         ...state,
         scanning: false,
-        error: action.payload.error
+        error: action.payload.error,
       };
     }
 
     case ANTIVIRUS_ACTION.GET_STATE_BEGIN: {
       return {
         ...state,
-        error: null
+        error: null,
       };
     }
 
     case ANTIVIRUS_ACTION.GET_STATE_SUCCESS: {
       return {
         ...state,
-        ...action.payload.data
+        ...action.payload.data,
       };
     }
 
     case ANTIVIRUS_ACTION.GET_STATE_FAILURE: {
       return {
         ...state,
-        error: action.payload.error
+        error: action.payload.error,
       };
     }
   }
