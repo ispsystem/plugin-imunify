8c3f41d43eaa7be7b0b6f517d08b9ec315d7e5bc
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-07-30 07:33:08 +0800

    feat: 🎸 create PreviewFree functional component
    Closes: #AS-1490

---
+++ client/packages/antivirus-card/src/components/preview/PreviewFree.tsx
@@ -0,0 +1,26 @@
+import { FunctionalComponent, h } from '@stencil/core';
+import { ImunifyLogo } from '../icons/ImunifyLogo';
+
+/**
+ * PreviewFree component props
+ */
+interface PreviewFreeProps {
+  title: string;
+  text: string;
+  onClick: (event: MouseEvent) => void;
+}
+
+/**
+ * Functional component
+ *
+ * @param props - properties
+ */
+export const PreviewFree: FunctionalComponent<PreviewFreeProps> = props => (
+  <section onClick={props.onClick.bind(this)} class="antivirus-card-preview__free">
+    <h4 class="antivirus-card-preview__free-title">{props.title}</h4>
+    <p class="antivirus-card-preview__free-msg">{props.text}</p>
+    <div class="antivirus-card-preview__free-icon">
+      <ImunifyLogo></ImunifyLogo>
+    </div>
+  </section>
+);
