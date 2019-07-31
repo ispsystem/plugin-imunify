a489aec78a063d6ae26e6c298770a6eb9ddb7174
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-07-31 18:01:07 +0800

    lint: 🚨 disable typescript rules for javascript files
    Closes: #AS-1492

--- client/commitlint.config.js
+++ client/commitlint.config.js
@@ -2,5 +2,10 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 80],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'refactor', 'fix', 'docs', 'test', 'perf', 'review', 'chore', 'style', 'wip', 'ci', 'release', 'lint', 'merge', 'mv'],
+    ],
   },
 };
