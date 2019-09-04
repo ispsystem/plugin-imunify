8842fa97bb84d2a678a8fb846064ddb66d089c70
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-09-04 10:54:31 +0800

    refactor(antivirus-card): 💡 remove unused functions

--- client/packages/antivirus-card/src/utils/tools.ts
+++ client/packages/antivirus-card/src/utils/tools.ts
@@ -3,57 +3,20 @@ import { PricePeriodType } from '../models/antivirus/model';
 import { ISPNotifier, ISPNotifierNotifyType } from '@ispsystem/notice-tools';
 
 /**
- * Combination of async function + await + setTimeout
- *
- * @param m - milliseconds
- */
-export const sleep = (m: number) => new Promise(r => setTimeout(r, m));
-
-/**
- * Выбор слова в соответствующем падеже для опеределенного числа
- *
- * @param num число для которого необходимо подобрать склонение
- * @param listMsgs список возможных слов для числа, состоит из 3-х элементов: (для единиц, для пар, для других чисел)
- */
-export function declOfNum(num: number, listMsgs: string[]): string {
-  if (num === 1) {
-    return listMsgs[0];
-  } else if (num % 100 > 4 && num % 100 < 20) {
-    return listMsgs[2];
-  } else {
-    switch (num % 10) {
-      case 1:
-        return listMsgs[listMsgs.length > 3 ? 3 : 0];
-      case 2:
-      case 3:
-      case 4:
-        return listMsgs[1];
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 0:
-        return listMsgs[2];
-    }
-  }
-}
-
-/**
  * Return nested object or undefined
  *
  * @param object - root object
  * @param pathArr - path to nested object as string array
  */
-export function getNestedObject(object: object, pathArr: string[]) {
+export function getNestedObject(object: object, pathArr: string[]): any {
   return pathArr.reduce((obj, key) => (obj && obj[key] !== 'undefined' ? obj[key] : undefined), object);
 }
 
 /**
- * Создание уникального идентификатора
+ * Create unique ID
  * https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
  */
-export function uuidv4() {
+export function uuidv4(): string {
   const hash: any = ([1e7] as any) + -1e3 + -4e3 + -8e3 + -1e11;
   // https://stackoverflow.com/questions/44042816/what-is-wrong-with-crypto-getrandomvalues-in-internet-explorer-11
   const crypto = window.crypto || window['msCrypto']; // for IE 11
@@ -66,8 +29,8 @@ export function uuidv4() {
  *
  * @param n - day or time number
  */
-export function pad(n: number) {
-  return n < 10 ? '0' + n : n;
+export function pad(n: number): string {
+  return n < 10 ? '0' + n : String(n);
 }
 
 /**
